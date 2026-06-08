9dce495555219991b72d985a3f789d698fc31717
pengchao2022 <pengchao.ma2@outlook.com>
2026-06-08 18:10:54 +0800

    feat: GTP-54273 added the api_gateway module

---
+++ modules/api_gateway/variables.tf
@@ -0,0 +1,17 @@
+variable "api_name" {
+  description = "The name of the API Gateway"
+  type        = string
+  
+}
+
+variable "lambda_invoke_arn" {
+  description = "The invoke ARN of the Lambda function"
+  type        = string
+  
+}
+
+variable "lambda_function_name" {
+  description = "The name of the Lambda function"
+  type        = string
+  
+}
